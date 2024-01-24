61450d5c43c206ba7ccc8f30b075c513d3858bf8
Gomez7373 <sgc.holberton@gmail.com>
2024-01-24 11:45:56 -0400

    8-concat_edges.py

--- python-hello_world/8-concat_edges.py
+++ python-hello_world/8-concat_edges.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
 str = "Python is an interpreted, interactive, object-oriented programming\
  language that combines remarkable power with very clear syntax"
-print(f"{str[39:-65]} {str[107:-18]} {str[:-123]}")
+str = f"{str[39:-64]}{str[106:-17]}{str[:-123]}"
+print(str)

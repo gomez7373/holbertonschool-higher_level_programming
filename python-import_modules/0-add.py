f8d1d6621fdfd6a77e066a7c0f71ee25b8e799a6
Gomez7373 <sgc.holberton@gmail.com>
2024-01-27 10:31:08 -0400

    0-add.py

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,11 +1,9 @@
 #!/usr/bin/python3
 
-if __name__ == "__main__"
+if __name__ == "__main__":
 
-from add_0 import add
+    from add_0 import add
 
-a = 1
-b = 2
-
-# Print the result using string format
-print("{} + {} = {}".format(a, b, add(a, b)))
+    a = 1
+    b = 2
+    print("{} + {} = {}".format(a, b, add(a, b)))

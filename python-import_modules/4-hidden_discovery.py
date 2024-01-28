12aede1adb84069c878a82c42319bcf6ffee33a1
Gomez7373 <sgc.holberton@gmail.com>
2024-01-27 23:01:01 -0400

    4-hidden_discovery.py

--- python-import_modules/4-hidden_discovery.py
+++ python-import_modules/4-hidden_discovery.py
@@ -1,10 +1,8 @@
 #!/usr/bin/python3
-
 if __name__ == "__main__":
-
     import hidden_4
 
     names = dir(hidden_4)
-    for name in names:
-        if name[:2] != "__":
-            print(name)
+    filtered_names = [name for name in names if not name.startswith("__")]
+    for name in sorted(filtered_names):
+        print(name)

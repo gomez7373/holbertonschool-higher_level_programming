461558081065d368e61ebfa66905721e00cbb241
Gomez7373 <sgc.holberton@gmail.com>
2024-01-31 12:39:04 -0400

    1-search_replace.py

--- python-more_data_structures/1-search_replace.py
+++ python-more_data_structures/1-search_replace.py
@@ -1,1 +1,7 @@
 #!/usr/bin/pythone
+def search_replace(my_list, search, replace):
+    new_list = my_list[:]
+    for i in range(len(new_list)):
+        if new_list[i] == search:
+            new_list = replace
+            return (new_list)

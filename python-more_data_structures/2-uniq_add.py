0cabb55c56ec6457591f13de249605115352e53c
Gomez7373 <sgc.holberton@gmail.com>
2024-02-01 10:07:06 -0400

    2-uniq_add.py

---
+++ python-more_data_structures/2-uniq_add.py
@@ -0,0 +1,13 @@
+#/usr/bin/python3
+
+def uniq_add(my_list=[]):
+    
+    result_set = set() 
+    result_sum = 0
+
+    for x in my_list:
+        if x not in result_set:
+            result_sum += x
+            result_set.add(x)
+        
+        return result_sum

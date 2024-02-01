b3e11cfcc424d71e291ce326c8154c3efaa1a906
Gomez7373 <sgc.holberton@gmail.com>
2024-02-01 17:30:29 -0400

    7-update_dictionary.py

--- python-more_data_structures/7-update_dictionary.py
+++ python-more_data_structures/7-update_dictionary.py
@@ -1,5 +1,4 @@
-#!/usr/bin/pythone3
-
+#!/usr/bin/python3
 def update_dictionary(a_dictionary, key, value):
-
-    return (a_dictionary.update({key: value}) or a_dictionary)
+    a_dictionary[key] = value
+    return a_dictionary

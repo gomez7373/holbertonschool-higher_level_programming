6868ae1b89dba8fc5be5f98d69ed0055e656d494
Gomez7373 <sgc.holberton@gmail.com>
2024-02-04 22:05:18 -0400

    2-matrix_divided.py

--- python-test_driven_development/2-matrix_divided.py
+++ python-test_driven_development/2-matrix_divided.py
@@ -1,26 +1,30 @@
-#!/usr/bin/python3
+#!/usr/bin/python3i
+
 
 def matrix_divided(matrix, div):
     """
     Divides all elements of a matrix.
 
     Prototype: def matrix_divided(matrix, div)
-    matrix must be a list of lists of integers or floats, otherwise raise a TypeError exception
-    with the message 'matrix must be a matrix (list of lists) of integers/floats'.
-    Each row of the matrix must be of the same size, otherwise raise a TypeError exception
+    matrix must be a list of lists of integers or floats, otherwise raise a
+    TypeError exception with the message 'matrix must be a matrix (list of
+    lists) of integers/floats'. Each row of the matrix must be of the
+    same size, otherwise raise a TypeError exception
     with the message 'Each row of the matrix must have the same size'.
-    div must be a number (integer or float), otherwise raise a TypeError exception
-    with the message 'div must be a number'.
+    div must be a number (integer or float), otherwise raise a TypeError
+    exception with the message 'div must be a number'.
     div can’t be equal to 0, otherwise raise a ZeroDivisionError exception
     with the message 'division by zero'.
-    All elements of the matrix should be divided by div, rounded to 2 decimal places.
+    All elements of the matrix should be divided by div,
+    rounded to 2 decimal places.
     Returns a new matrix.
     You are not allowed to import any module
     """
 
     # Check if matrix is a list of lists of integers or floats
-    if not all(isinstance(row, list) and all(isinstance(num, (int, float)) for num in row) for row in matrix):
-        raise TypeError('matrix must be a matrix (list of lists) of integers/floats')
+    if not all(isinstance(row, list) and all(isinstance(num, (int, float))
+        for num in row) for row in matrix):
+        raise TypeError('matrix will be matrix (list of lists) of int/flot')
 
     # Check if each row has the same size
     if any(len(row) != len(matrix[0]) for row in matrix):

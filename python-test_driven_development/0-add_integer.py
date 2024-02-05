3a3c9ea826992d1c79f3bee07454abb0065436b2
Gomez7373 <sgc.holberton@gmail.com>
2024-02-04 22:11:48 -0400

    2-matrix_divided.py

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -6,7 +6,8 @@ def add_integer(a, b=98):
     Adds two integers.
 
     Prototype: def add_integer(a, b=98)
-    a and b must be integers or floats, otherwise raise a TypeError exception with the message
+    a and b must be integers or floats, otherwise raise a
+    TypeError exception with the message
     'a must be an integer' or 'b must be an integer'.
     a and b must be first casted to integers if they are float.
     Returns an integer: the addition of a and b

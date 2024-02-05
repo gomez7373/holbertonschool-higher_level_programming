cb3c07d84b830bfc79765bebab539fbed33e97e6
Gomez7373 <sgc.holberton@gmail.com>
2024-02-04 22:19:49 -0400

    4-print_square.py

--- python-test_driven_development/3-say_my_name.py
+++ python-test_driven_development/3-say_my_name.py
@@ -1,31 +1,23 @@
 #!/usr/bin/python3
+"""Module for say_my_name method."""
 
 
 def say_my_name(first_name, last_name=""):
-    """
-    Prints "My name is <first name> <last name>"
+    """Method for printing first and last name.
 
-    Prototype: def say_my_name(first_name, last_name="")
-    first_name and last_name must be strings otherwise, raise a TypeError exception
-    with the message 'first_name must be a string or last_name must be a string'.
-    You are not allowed to import any module
-    """
+    Args:
+        first_name: first name string.
+        last_name: last name string.
 
-    # Check if first_name is a string
+    Raises:
+        TypeError: If first_name or last_name are not strings.
+    """
     if not isinstance(first_name, str):
-        raise TypeError('first_name must be a string')
+        raise TypeError("first_name must be a string")
 
-    # Check if last_name is a string
     if not isinstance(last_name, str):
-        raise TypeError('last_name must be a string')
+        raise TypeError("last_name must be a string")
 
-    # Print the formatted string
-    print("My name is {} {}".format(first_name, last_name))
+    print("My name is {:s} {:s}".format(first_name, last_name))
 
-# Example usage
-say_my_name("John", "Smith")
-say_my_name("Walter", "White")
-say_my_name("Bob")
 
-# Uncomment the following line to see the exception message
-# say_my_name(12, "White")

5ed33957ba112a911bda9e5b27ebdd57bcafe301
Gomez7373 <sgc.holberton@gmail.com>
2024-02-06 19:16:22 -0400

    1-rectangle.py

---
+++ python-more_classes/1-rectangle.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+
+
+class Rectangle:
+    """A class to represent a rectangle."""
+
+    def __init__(self, width=0, height=0):
+        """Initialize a Rectangle instance with optional width and height."""
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        """int: The width of the rectangle."""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """Set the width of the rectangle.
+
+        Raises:
+            TypeError: If the value is not an integer.
+            ValueError: If the value is less than 0.
+        """
+
+    @property
+    def height(self):
+        """int: The height of the rectangle."""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """Set the height of the rectangle.
+
+        Raises:
+            TypeError: If the value is not an integer.
+            ValueError: If the value is less than 0.
+        """
---
+++ python-more_classes/readme.md
@@ -0,0 +1,8 @@
+issue
+
+$ ~/.local/bin/pycodestyle 1-rectangle.py
+
+$ export PATH="$PATH:$HOME/.local/bin"
+
+$ pycodestyle 1-filename.py
+

25ca66b63926f340d723c8dab5372854123aac5e
Gomez7373 <sgc.holberton@gmail.com>
2024-02-11 22:19:43 -0400

    terminal

--- python-inheritance/10-square.py
+++ python-inheritance/10-square.py
@@ -1,4 +1,14 @@
 #!/usr/bin/python3
+"""
+
+   Class BaseGeometry.
+   Public instance method: \
+   def area(self): that raises an Exception with\
+   the message area() is not implemented.
+   Public instance method:\
+   def integer_validator(self, name, value):\
+   that validates value
+"""
 
 
 class BaseGeometry:
@@ -14,7 +24,7 @@ class BaseGeometry:
         """Public instance method:\
            def integer_validator(self, name, value):\
            that validates value"""
-        if type(value) != int:
+        if not isinstance(value, int):
             raise TypeError("{:s} must be an integer".format(name))
         if value <= 0:
             raise ValueError("{:s} must be greater than 0".format(name))

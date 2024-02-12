32780fde32515a016b3fc3ef5ba188bec3854041
Gomez7373 <sgc.holberton@gmail.com>
2024-02-11 22:17:48 -0400

    terminal

--- python-inheritance/11-square.py
+++ python-inheritance/11-square.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-   6-base_geometry.py module
+
    Class BaseGeometry.
    Public instance method: \
    def area(self): that raises an Exception with\
@@ -24,7 +24,7 @@ class BaseGeometry:
         """Public instance method:\
            def integer_validator(self, name, value):\
            that validates value"""
-        if type(value) != int:
+        if not isinstance(value, int):
             raise TypeError("{:s} must be an integer".format(name))
         if value <= 0:
             raise ValueError("{:s} must be greater than 0".format(name))

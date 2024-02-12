2e1080fb31473400004efa6c211dd068a5459954
Gomez7373 <sgc.holberton@gmail.com>
2024-02-11 23:46:36 -0400

    terminal

--- python-inheritance/7-base_geometry.py
+++ python-inheritance/7-base_geometry.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python3
+ #!/usr/bin/python3
 """This module creates a class named BaseGeometry"""
 
 
@@ -14,7 +14,7 @@ class BaseGeometry:
 
     def integer_validator(self, name, value):
         """validates input"""
-        if not isinstance(value, int):
+        if type(value) != int:
             raise TypeError("{} must be an integer".format(name))
         if value <= 0:
             raise ValueError("{} must be greater than 0".format(name))

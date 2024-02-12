a6f3828d47b804ad1a3a9b6ec24ec1630a34dcfa
Gomez7373 <sgc.holberton@gmail.com>
2024-02-11 22:10:16 -0400

    terminal

--- python-inheritance/11-square.py
+++ python-inheritance/11-square.py
@@ -1,4 +1,14 @@
 #!/usr/bin/python3
+"""
+   6-base_geometry.py module
+   Class BaseGeometry.
+   Public instance method: \
+   def area(self): that raises an Exception with\
+   the message area() is not implemented.
+   Public instance method:\
+   def integer_validator(self, name, value):\
+   that validates value
+"""
 
 
 class BaseGeometry:

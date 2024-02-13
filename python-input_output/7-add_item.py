0af2ff270300bb75a44d0018f246c0509bd74a34
Gomez7373 <sgc.holberton@gmail.com>
2024-02-12 22:47:53 -0400

    terminal

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,18 +1,15 @@
 #!/usr/bin/python3
-
-
+"""Add all arguments to a Python list and save them to a file."""
 import sys
-from save_to_json_file import save_to_json_file
-from load_from_json_file import load_from_json_file
 
-# Get the existing items from the file if it exists
-try:
-    items = load_from_json_file("add_item.json")
-except FileNotFoundError:
-    items = []
+if __name__ == "__main__":
+    save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+    load_from_json_file = \
+        __import__('6-load_from_json_file').load_from_json_file
 
-# Append the new items from the command line arguments
-items.extend(sys.argv[1:])
-
-# Save the updated list to the file
-save_to_json_file(items, "add_item.json")
+    try:
+        items = load_from_json_file("add_item.json")
+    except FileNotFoundError:
+        items = []
+    items.extend(sys.argv[1:])
+    save_to_json_file(items, "add_item.json")

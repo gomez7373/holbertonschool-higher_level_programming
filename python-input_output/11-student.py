8328e7076f52bc14f9a503553958527679021e54
Gomez7373 <sgc.holberton@gmail.com>
2024-02-12 23:38:49 -0400

    terminal

--- python-input_output/11-student.py
+++ python-input_output/11-student.py
@@ -1,22 +1,32 @@
 #!/usr/bin/python3
-"""Defines a class Student."""
+"""Student module.
+
+Contains a Student class and some methods.
+"""
 
 
-class Student:
-    """Represent a student."""
+class Student():
+    """Defines a Student."""
 
     def __init__(self, first_name, last_name, age):
-        """Initialize a new Student.
+        """Sets the necessary attributes for the Student object.
 
         Args:
-            first_name (str): The first name of the student.
-            last_name (str): The last name of the student.
-            age (int): The age of the student.
+            first_name (str): first name of the student.
+            last_name (str): last name of the student.
+            age (int): age of the student.
         """
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
-    def to_json(self):
-        """Get a dictionary representation of the Student."""
+    def to_json(self, attrs=None):
+        """Retrieves a dictionary representation of a Student instance."""
+        if attrs is not None:
+            return {k: v for k, v in self.__dict__.items() if k in attrs}
         return self.__dict__
+
+    def reload_from_json(self, json):
+        """Replaces all attributes of the Student instance."""
+        for k, v in json.items():
+            setattr(self, k, v)

04624c61cb4ece4308c88c08e1386d74d8e1124f
Gomez7373 <sgc.holberton@gmail.com>
2024-02-12 23:39:35 -0400

    terminal

--- python-input_output/12-pascal_triangle.py
+++ python-input_output/12-pascal_triangle.py
@@ -1,21 +1,24 @@
 #!/usr/bin/python3
-"""Defines a Pascal's Triangle function."""
+"""pascal_triangle module.
+
+Contains a function that returns a list of lists of
+integers representing the Pascal’s triangle of n.
+"""
 
 
 def pascal_triangle(n):
- """
+    """
     Returns a list of lists of integers
     representing the Pascal’s triangle of n.
     """
     if n <= 0:
         return []
 
-    triangles = [[1]]
-    while len(triangles) != n:
-        tri = triangles[-1]
+    result = [[1]]
+    while len(result) is not n:
         tmp = [1]
-        for i in range(len(tri) - 1):
-            tmp.append(tri[i] + tri[i + 1])
+        for i in range(len(result[-1]) - 1):
+            tmp.append(result[-1][i] + result[-1][i + 1])
         tmp.append(1)
-        triangles.append(tmp)
-    return triangles
+        result.append(tmp)
+    return result

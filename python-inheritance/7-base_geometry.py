9ffa22ab68d61b7f73f1f011f1cba842b592b652
gomez7373 <sgc.holberton@gmail.com>
2024-05-22 21:24:02 -0400

    7-base_geometry.py

--- python-inheritance/7-base_geometry.py
+++ python-inheritance/7-base_geometry.py
@@ -1,19 +1,21 @@
 #!/usr/bin/python3
 """This module creates a class named BaseGeometry"""
 
+
 class BaseGeometry:
-    """An class named BaseGeometry
+    """A class named BaseGeometry
 
     Attributes:
     attr1(area): Raises an exception
     """
+
     def area(self):
-        """raises an exception"""
+        """Raises an exception"""
         raise Exception("area() is not implemented")
 
     def integer_validator(self, name, value):
-        """validates input"""
-        if type(value) != int:
+        """Validates input"""
+        if not isinstance(value, int):
             raise TypeError("{} must be an integer".format(name))
         if value <= 0:
             raise ValueError("{} must be greater than 0".format(name))

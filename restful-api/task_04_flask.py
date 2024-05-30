15fefbb9ead4af3631bbcedb703fa62b3569f814
gomez7373 <sgc.holberton@gmail.com>
2024-05-30 17:38:52 -0400

    task_04_flask

--- restful-api/task_04_flask.py
+++ restful-api/task_04_flask.py
@@ -5,6 +5,7 @@ Module for a simple Flask API.
 
 from typing import Dict, Union  # Standard library import should come first
 from flask import Flask, jsonify, request, Response
+
 # Third-party library import, line above.
 
 app = Flask(__name__)
@@ -13,26 +14,26 @@ app = Flask(__name__)
 users: Dict[str, Dict[str, Union[str, int]]] = {}
 
 
-@app.route('/')
+@app.route("/")
 def home() -> str | Response:
     """Route for the root URL."""
     return "Welcome to the Flask API!"
 
 
-@app.route('/data')
+@app.route("/data")
 def get_data() -> str | Response:
     """Route to return a JSON response
     with a list of all usernames stored in the API."""
     return jsonify(list(users.keys()))
 
 
-@app.route('/status')
+@app.route("/status")
 def status() -> str | Response:
     """Route to return 'OK'."""
     return "OK"
 
 
-@app.route('/users/<username>')
+@app.route("/users/<username>")
 def get_user(username: str) -> str | Response:
     """Route to return the full object
     corresponding to the provided username."""
@@ -41,13 +42,13 @@ def get_user(username: str) -> str | Response:
     return jsonify({"error": "User not found"}), 404
 
 
-@app.route('/add_user', methods=['POST'])
+@app.route("/add_user", methods=["POST"])
 def add_user() -> str | Response:
     """Route to add a new user to the API."""
     data: Dict[str, Union[str, int]] = request.json
     if not data:
         return jsonify({"error": "No data provided"}), 400
-    username = data.get('username')
+    username = data.get("username")
     if not username:
         return jsonify({"error": "Username is required"}), 400
     if username in users:

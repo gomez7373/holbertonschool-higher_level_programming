97c077bc4f82d127132d3f632a4c20e99388a51e
gomez7373 <sgc.holberton@gmail.com>
2024-06-17 17:46:44 -0400

    task 1

---
+++ SQL_introduction/1-create_database_if_missing.sql
@@ -0,0 +1,2 @@
+-- This will only create data base if it dosen't exist.
+CREATE DATABASE IF NOT EXISTS hbtn_0c_0;
---
+++ SQL_introduction/in_AND_out.md
@@ -0,0 +1,7 @@
+how to enter:
+mysql -u root -p
+
+password:
+
+how to exit:
+ctrl + d

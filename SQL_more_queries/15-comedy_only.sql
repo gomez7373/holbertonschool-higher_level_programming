15bd73d44df09f10334bfb8c884a02323e686e54
gomez7373 <sgc.holberton@gmail.com>
2024-06-18 16:55:05 -0400

    task16

---
+++ SQL_more_queries/15-comedy_only.sql
@@ -0,0 +1,9 @@
+-- List all Comedy shows in the hbtn_0d_tvshows database
+
+SELECT s.title
+FROM tv_shows s
+JOIN tv_show_genres sg ON s.id = sg.show_id
+JOIN tv_genres g ON sg.genre_id = g.id
+WHERE g.name = 'Comedy'
+ORDER BY s.title ASC;
+

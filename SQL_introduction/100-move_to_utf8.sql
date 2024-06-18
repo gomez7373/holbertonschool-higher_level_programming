7219dd9963216277c473497a7b71021c9952fa6d
gomez7373 <sgc.holberton@gmail.com>
2024-06-17 22:26:01 -0400

    rm

--- SQL_introduction/100-move_to_utf8.sql
+++
@@ -1,8 +0,0 @@
--- Convert the database hbtn_0c_0 to UTF8 (utf8mb4, collate utf8mb4_unicode_ci)
-ALTER DATABASE hbtn_0c_0 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci;
-
--- Convert the table first_table to UTF8 (utf8mb4, collate utf8mb4_unicode_ci)
-ALTER TABLE hbtn_0c_0.first_table CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
-
--- Convert the field name in first_table to UTF8 (utf8mb4, collate utf8mb4_unicode_ci)
-ALTER TABLE hbtn_0c_0.first_table MODIFY name VARCHAR(256) COLLATE utf8mb4_unicode_ci;

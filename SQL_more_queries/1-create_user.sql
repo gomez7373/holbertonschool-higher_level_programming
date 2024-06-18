9f72146d2bb7cd5dd6d267612a24078dcb683d07
gomez7373 <sgc.holberton@gmail.com>
2024-06-18 12:29:52 -0400

    task1

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,8 +1,9 @@
--- Create the user if it doesn't already exist
+-- script creates the MySQL server user user_0d_1.
+--user_0d_1 should have all privileges on your MySQL server
+--The user_0d_1 password should be set to user_0d_1_pwd
+--If the user user_0d_1 already exists, your script should not fail
+
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
-
--- Grant all privileges to the user
 GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost' WITH GRANT OPTION;
-
--- Ensure the privileges are flushed so that they take effect immediately
 FLUSH PRIVILEGES;
+

6a1e713ad264a5e64aa346c365fdfcbf7f48e484
gomez7373 <sgc.holberton@gmail.com>
2024-06-17 23:07:32 -0400

    100

--- SQL_introduction/100-move_to_utf8.sql
+++ SQL_introduction/100-move_to_utf8.sql
@@ -1,5 +1,12 @@
--- This script that converts hbtn_0c_0 database to UTF8
+-- Change character SET and COLLATE for database
 ALTER DATABASE hbtn_0c_0 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci;
-USE hbtn_0c_0
+
+-- Use the database
+USE hbtn_0c_0;
+
+-- Modify table `first_table` to utf8mb4 character set
 ALTER TABLE first_table CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
-ALTER TABLE first_table CHANGE name name VARCHAR (256) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- Change column `name` to utf8mb4 character set
+ALTER TABLE first_table MODIFY name VARCHAR(256) COLLATE utf8mb4_unicode_ci;
+

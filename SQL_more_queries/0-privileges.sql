2abcf2570ccbe88dcf8d4d505ded193c00666ac7
gomez7373 <sgc.holberton@gmail.com>
2024-06-18 11:40:30 -0400

    0

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -1,4 +1,3 @@
 -- This will print all  privileges of the USER.
--- more comments.
-SHOW GRANTS FOR 'user_0d_1`@`localhost';
-SHOW GRANTS FOR 'user_0d_2`@`localhost';
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
+SHOW GRANTS FOR 'user_0d_2'@'localhost';

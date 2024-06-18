f44876c36ece1b044096a3a15f5afff664be1b3e
gomez7373 <sgc.holberton@gmail.com>
2024-06-17 21:33:45 -0400

    task16

--- SQL_introduction/15-groups.sql
+++ SQL_introduction/15-groups.sql
@@ -1,2 +1,3 @@
--- This script that lists the number of records with the same score in the table.
-SELECT score, COUNT(score) AS NUMBER FROM second_table GROUP BY score ORDER BY score DESC;
+-- This script lists the number of records with the same score in the table.
+SELECT score, COUNT(score) AS number FROM second_table GROUP BY score ORDER BY score DESC;
+

62ccd12dc0f0c90add699184d2f110bdcaab3413
gomez7373 <sgc.holberton@gmail.com>
2024-06-19 15:47:14 -0400

    task2

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -1,33 +1,49 @@
 #!/usr/bin/python3
 """
-This script takes in an argument and displays all values in the states table
-of hbtn_0e_0_usa where name matches the argument.
-It takes 4 arguments: mysql username, mysql password, database name, and state name.
+This script takes in an argument and displays all values in the states
+table of hbtn_0e_0_usa where name matches the argument.
+It takes 4 arguments: mysql username, mysql password, database name,
+and state name.
 """
 
-import sys
 import MySQLdb
+from getpass import getpass
+from sys import argv
 
 if __name__ == "__main__":
+    # Capture the command-line arguments
+    mysql_username = argv[1]
+    mysql_password = getpass(prompt="Enter MySQL password: ")
+    database_name = argv[2]
+    state_name = argv[3]
+
     try:
         # Connect to the MySQL database
         database = MySQLdb.connect(
             host="localhost",
-            user=sys.argv[1],
-            passwd=sys.argv[2],
-            db=sys.argv[3],
-            port=3306
+            user=mysql_username,
+            passwd=mysql_password,
+            db=database_name,
+            port=3306,
         )
 
+        # Create a cursor to interact with the database
         with database.cursor() as cursor:
-            cursor.execute(
-                    "SELECT * FROM states WHERE name = %s ORDER BY id ASC",
-                    (sys.argv[4],))
-            result = cursor.fetchall()
-            if result:
-                print(*result, sep="\n")
+            # Create the SQL query using f-string
+            query = (
+                f"SELECT * FROM states WHERE name = '{state_name}' "
+                "ORDER BY id ASC"
+            )
+            cursor.execute(query)
 
-        # Close the connection
+            # Fetch all the matching rows
+            results = cursor.fetchall()
+
+            # Print the results
+            for row in results:
+                print(row)
+
+        # Close the database connection
         database.close()
 
     except MySQLdb.OperationalError as e:

e021222a1fa3bcfb6902bf65e3b27483e6c86bd9
gomez7373 <sgc.holberton@gmail.com>
2024-06-19 19:24:33 -0400

    task4

--- python-object_relational_mapping/4-cities_by_state.py
+++ python-object_relational_mapping/4-cities_by_state.py
@@ -4,27 +4,30 @@ This script lists all cities from the database hbtn_0e_4_usa.
 It takes 3 arguments: mysql username, mysql password, and database name.
 """
 
-import MySQLdb
-from sys import argv
+import sys
 from getpass import getpass
 
+import MySQLdb
+
 if __name__ == "__main__":
-    # Check for correct number of arguments
-    if len(argv) != 4:
-        print("Usage: ./4-cities_by_state.py <mysql username> <mysql password> <database name>")
-        exit(1)
+    # Ensure the correct number of arguments are provided
+    if len(sys.argv) != 4:
+        print(
+            "Usage: ./4-cities_by_state.py <mysql username> <mysql password> <database name>"
+        )
+        sys.exit(1)
 
     # Capture the command-line arguments
-    mysql_username = argv[1]
-    mysql_password = getpass(prompt="Enter MySQL password: ")
-    database_name = argv[3]
+    MYSQL_USERNAME = sys.argv[1]
+    MYSQL_PASSWORD = getpass(prompt="Enter MySQL password: ")
+    DATABASE_NAME = sys.argv[3]
 
     # Connect to the MySQL database
     database = MySQLdb.connect(
         host="localhost",
-        user=mysql_username,
-        passwd=mysql_password,
-        db=database_name,
+        user=MYSQL_USERNAME,
+        passwd=MYSQL_PASSWORD,
+        db=DATABASE_NAME,
         port=3306,
     )
 

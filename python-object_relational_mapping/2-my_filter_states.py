36e40b402be696e50f4dc1a76ca053774f907c3f
gomez7373 <sgc.holberton@gmail.com>
2024-06-19 15:20:27 -0400

    task2

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -2,27 +2,33 @@
 """
 This script takes in an argument and displays all values in the states table
 of hbtn_0e_0_usa where name matches the argument.
-It takes 4 arguments: mysql username, mysql password,
-database name, and state name.
+It takes 4 arguments: mysql username, mysql password, database name, and state name.
 """
 
-from sys import argv
-
+import sys
 import MySQLdb
 
 if __name__ == "__main__":
-    # Connect to the MySQL database
-    database = MySQLdb.connect(
-        host="localhost", user=argv[1], passwd=argv[2], db=argv[3], port=3306
-    )
-
-    with database.cursor() as cursor:
-        cursor.execute(
-            "SELECT * FROM states WHERE name = %s ORDER BY id ASC", (argv[4],)
+    try:
+        # Connect to the MySQL database
+        database = MySQLdb.connect(
+            host="localhost",
+            user=sys.argv[1],
+            passwd=sys.argv[2],
+            db=sys.argv[3],
+            port=3306
         )
-        result = cursor.fetchall()
-        if result:
-            print(*result, sep="\n")
 
-    # Close the connection
-    database.close()
+        with database.cursor() as cursor:
+            cursor.execute(
+                    "SELECT * FROM states WHERE name = %s ORDER BY id ASC",
+                    (sys.argv[4],))
+            result = cursor.fetchall()
+            if result:
+                print(*result, sep="\n")
+
+        # Close the connection
+        database.close()
+
+    except MySQLdb.OperationalError as e:
+        print(f"Error connecting to the database: {e}")

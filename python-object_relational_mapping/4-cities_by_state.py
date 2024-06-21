ece191f1fca1d4a5cff9033edab0e3cf961ce7f2
gomez7373 <sgc.holberton@gmail.com>
2024-06-20 21:03:24 -0400

    task4

--- python-object_relational_mapping/4-cities_by_state.py
+++ python-object_relational_mapping/4-cities_by_state.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 """
-Lists all cities from the database hbtn_0e_4_usa.
-Arguments: MySQL username, MySQL password, and database name.
+This script lists all cities from the database hbtn_0e_4_usa.
+
+It takes 3 arguments: mysql username, mysql password, and database name.
 """
 
 import sys
@@ -9,30 +10,34 @@ from getpass import getpass
 import MySQLdb
 
 if __name__ == "__main__":
+    # Capture the command-line arguments
     MYSQL_USERNAME = sys.argv[1]
     MYSQL_PASSWORD = getpass(prompt="Enter MySQL password: ")
     DATABASE_NAME = sys.argv[3]
 
-    try:
-        database = MySQLdb.connect(
-            host="localhost",
-            user=MYSQL_USERNAME,
-            passwd=MYSQL_PASSWORD,
-            db=DATABASE_NAME,
-            port=3306
+    # Connect to the MySQL database
+    database = MySQLdb.connect(
+        host="localhost",
+        user=MYSQL_USERNAME,
+        passwd=MYSQL_PASSWORD,
+        db=DATABASE_NAME,
+        port=3306,
+    )
+
+    with database.cursor() as cursor:
+        cursor.execute(
+            "SELECT cities.id, cities.name, states.name FROM cities "
+            "JOIN states ON cities.state_id = states.id "
+            "ORDER BY cities.id ASC"
         )
+        result = cursor.fetchall()
+        if result:
+            # Use a set to avoid duplicate prints
+            printed_rows = set()
+            for row in result:
+                if row not in printed_rows:
+                    print(row)
+                    printed_rows.add(row)
 
-        with database.cursor() as cursor:
-            cursor.execute(
-                "SELECT cities.id, cities.name, states.name FROM cities "
-                "JOIN states ON cities.state_id = states.id "
-                "ORDER BY cities.id ASC"
-            )
-            result = cursor.fetchall()
-            if result:
-                print(*result, sep="\n")
-
-        database.close()
-
-    except MySQLdb.OperationalError as e:
-        print(f"Error connecting to the database: {e}")
+    # Close the database connection
+    database.close()

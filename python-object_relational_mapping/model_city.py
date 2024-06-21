0792d2f7e1099176db830b26a56cdac0b4490c11
gomez7373 <sgc.holberton@gmail.com>
2024-06-21 15:13:10 -0400

    fixing long lines and extra lines added and ones missing

--- python-object_relational_mapping/model_city.py
+++ python-object_relational_mapping/model_city.py
@@ -4,6 +4,7 @@ This module defines the City class.
 """
 
 from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy.orm import relationship
 from model_state import Base
 
 class City(Base):
@@ -15,3 +16,27 @@ class City(Base):
     id = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
     name = Column(String(128), nullable=False)
     state_id = Column(Integer, ForeignKey('states.id'), nullable=False)
+
+    # Comment out the following line for other tasks to avoid conflicts.
+    state = relationship("State", back_populates="cities")
+
+# the above code works for the tasks before the task 14
+#remember to change it from test to code
+
+"""
+This module defines the City class.
+"""
+"""
+from sqlalchemy import Column, Integer, String, ForeignKey
+from model_state import Base
+
+class City(Base):
+    
+   # City class that represents the cities table.
+
+    __tablename__ = 'cities'
+
+    id = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
+    name = Column(String(128), nullable=False)
+    state_id = Column(Integer, ForeignKey('states.id'), nullable=False)
+"""
--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -4,6 +4,7 @@ This module defines a State class and a Base instance for SQLAlchemy.
 """
 
 from sqlalchemy import Column, Integer, String
+from sqlalchemy.orm import relationship
 from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
@@ -17,3 +18,28 @@ class State(Base):
 
     id = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
     name = Column(String(128), nullable=False)
+
+    # Comment out the following line for other tasks to avoid conflicts.
+    cities = relationship("City", backref="state", cascade="all, delete-orphan")
+
+"""
+This module defines a State class and a Base instance for SQLAlchemy.
+"""
+'''
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+
+class State(Base):
+    """
+    State class that represents the states table.
+    """
+    __tablename__ = 'states'
+
+    id = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
+    name = Column(String(128), nullable=False)
+'''
+# the upper code works for the tasks before the task 14
+#remember to change it from test to code 

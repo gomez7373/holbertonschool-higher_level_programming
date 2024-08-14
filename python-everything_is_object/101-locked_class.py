8e27d41e8a7f210dbd06e7b3fc4ad014249178b4
gomez7373 <sgc.holberton@gmail.com>
2024-08-13 21:42:17 -0400

    files

--- python-everything_is_object/101-locked_class.py
+++ python-everything_is_object/101-locked_class.py
@@ -1,2 +1,19 @@
+#!/usr/bin/python3
+"""
+This module defines a LockedClass that restricts dynamic attribute creation.
+"""
+
+
 class LockedClass:
+    """
+    A class that prevents the user from dynamically creating new instance
+    attributes, except if the new instance attribute is called 'first_name'.
+    """
+
     __slots__ = ['first_name']
+
+    def __init__(self):
+        """
+        Initialize the LockedClass instance.
+        """
+        pass
--- python-everything_is_object/101-main.py
+++ python-everything_is_object/101-main.py
@@ -1,13 +1,9 @@
 #!/usr/bin/python3
-# pylint: disable=invalid-name
-"""
-This module defines a LockedClass that restricts dynamic attribute creation.
-"""
+LockedClass = __import__('101-locked_class').LockedClass
 
-
-class LockedClass:
-    """
-    This class prevents the user from dynamically creating new instance
-    attributes, except if the new instance attribute is called first_name.
-    """
-    __slots__ = ['first_name']
+lc = LockedClass()
+lc.first_name = "John"
+try:
+    lc.last_name = "Snow"
+except Exception as e:
+    print("[{}] {}".format(e.__class__.__name__, e))
